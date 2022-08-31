4a3bf72883ff9b856fe7e3a3c288db5aa4e6d87a
Ruud Schramp <schramp@holmes.nl>
2022-08-31 17:25:46 +0200

    Remove utc reference from module

--- winfiletime/__init__.py
+++ winfiletime/__init__.py
@@ -1,3 +1,3 @@
-from .filetime import EPOCH_AS_FILETIME, from_datetime, to_datetime, utc
+from .filetime import EPOCH_AS_FILETIME, from_datetime, to_datetime
 
-__all__ = ["EPOCH_AS_FILETIME", "from_datetime", "to_datetime", "utc"]
+__all__ = ["EPOCH_AS_FILETIME", "from_datetime", "to_datetime"]
